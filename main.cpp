c2a6744f539bb15098aa1518d7466f91b7235188
Simon Pena Pereira <simon.pereira@gmx.de>
2021-03-03 08:27:57 +0100

    changed signed_volume function; deleted bbox in frow function

--- main.cpp
+++ main.cpp
@@ -17,64 +17,161 @@
 #include <stdio.h>
 
 float signed_volume(const Point &a, const Point &b, const Point &c, const Point &d) {
-  // to do
-    return (((a-d)*((b-d)*(c-d))/6));
+    return ((a-b).dot((b-d).cross(c-d)))/6;
 }
 
-bool intersects(const Point &orig, const Point &dest, const Point &v0, const Point &v1, const Point &v2) {
-    if signed_volume() > 0 on one side && < 0 on other side {
-        if()//next if statement
-    }
-    else go to next voxel-face combination
+const Point target1(float voxel_size, int i, int j, int k, float min_x, float min_y, float min_z) {
+    Point orgBB;
+    Point orig;
+    Point dest;
 
-    return 0;
+    orgBB.x = min_x;
+    orgBB.y = min_y;
+    orgBB.z = min_z;
+
+    orig.x = orgBB.x + i * voxel_size;
+    orig.y = orgBB.y + j * voxel_size + 0.5 * voxel_size;
+    orig.z = orgBB.z + k * voxel_size + 0.5 * voxel_size;
+    dest.x = orig.x + voxel_size;
+    dest.y = orig.y;
+    dest.z = orig.z;
+
+    return orig, dest;
 }
 
-int main(int argc, const char* argv[]) {
-    // I have added the path to the obj file. Local path so static and not dynamic. 
-    const char* file_in = "C:/Users/s161887/Documents/GitHub/geo1004_hw01_test/geo1004_hw01_test/bag_bk.obj";
-    const char* file_out = "vox.obj";
-    float voxel_size = 1.0;
+const Point target2(float voxel_size, int i, int j, int k, float min_x, float min_y, float min_z) {
+    Point orgBB;
+    Point orig;
+    Point dest;
 
-    // Read file
-    std::vector<Point> vertices;
-    std::vector<std::vector<unsigned int>> faces;
-    std::string line;
-    //std::string type;
-    std::ifstream in(file_in, std::ios::in);
+    orgBB.x = min_x;
+    orgBB.y = min_y;
+    orgBB.z = min_z;
 
-    if (!in)
-    {
-        std::cerr << "Cannot open" << file_in << std::endl;
-        exit(1);
+    orig.x = orgBB.x + i * voxel_size + 0.5 * voxel_size;
+    orig.y = orgBB.y + j * voxel_size;
+    orig.z = orgBB.z + k * voxel_size + 0.5 * voxel_size;
+    dest.x = orig.x;
+    dest.y = orig.y + voxel_size;
+    dest.z = orig.z;
+
+    return orig, dest;
+}
+
+const Point target3(float voxel_size, int i, int j, int k, float min_x, float min_y, float min_z) {
+    Point orgBB;
+    Point orig;
+    Point dest;
+
+    orgBB.x = min_x;
+    orgBB.y = min_y;
+    orgBB.z = min_z;
+
+    orig.x = orgBB.x + i * voxel_size + 0.5 * voxel_size;
+    orig.y = orgBB.y + j * voxel_size + 0.5 * voxel_size;
+    orig.z = orgBB.z + k * voxel_size;
+    dest.x = orig.x;
+    dest.y = orig.y;
+    dest.z = orig.z + voxel_size;
+
+    return orig, dest;
+}
+
+//funtion returning no_x, no_y, no_z
+//with input v0, v1, v2
+
+Rows face_rows(const Point& vf0, const Point& vf1, const Point& vf2, float voxel_size) {
+
+    std::vector<Point> fvertices;
+
+    fvertices.emplace_back(vf0.x, vf0.y, vf0.z);
+    fvertices.emplace_back(vf1.x, vf1.y, vf1.z);
+    fvertices.emplace_back(vf2.x, vf2.y, vf2.z);
+
+    std::vector<float> x_dim;
+    std::vector<float> y_dim;
+    std::vector<float> z_dim;
+
+    for (std::vector<Point>::const_iterator i = fvertices.begin(); i != fvertices.end(); ++i) {
+        x_dim.push_back(i[0][0]);
+        y_dim.push_back(i[0][1]);
+        z_dim.push_back(i[0][2]);
     }
 
-    while (std::getline(in, line))
-    {
-        std::istringstream vert(line);
-        vert >> line;
-        if (line == "v") {
-            float x, y, z;
-            vert >> x >> y >> z;
-            vertices.emplace_back(x, y, z);
-        }
-        if (line == "f") {
-            unsigned int f1, f2, f3;
-            vert >> f1 >> f2 >> f3;
-            std::vector<unsigned int> temp = { f1, f2, f3 };
-            faces.push_back(temp);
-        }
-    }
+    std::vector<float>::iterator result_maxx;
+    std::vector<float>::iterator result_maxy;
+    std::vector<float>::iterator result_maxz;
 
-    //Obtaining the Bounding Box
+    std::vector<float>::iterator result_minx;
+    std::vector<float>::iterator result_miny;
+    std::vector<float>::iterator result_minz;
+
+    result_maxx = std::max_element(x_dim.begin(), x_dim.end());
+    result_maxy = std::max_element(y_dim.begin(), y_dim.end());
+    result_maxz = std::max_element(z_dim.begin(), z_dim.end());
+
+    result_minx = std::min_element(x_dim.begin(), x_dim.end());
+    result_miny = std::min_element(y_dim.begin(), y_dim.end());
+    result_minz = std::min_element(z_dim.begin(), z_dim.end());
+
+    int index_x = std::distance(x_dim.begin(), result_maxx);
+    int index_y = std::distance(y_dim.begin(), result_maxy);
+    int index_z = std::distance(z_dim.begin(), result_maxz);
+
+    int index_minx = std::distance(x_dim.begin(), result_minx);
+    int index_miny = std::distance(y_dim.begin(), result_miny);
+    int index_minz = std::distance(z_dim.begin(), result_minz);
+
+    //vector indexing
+    float fmin_x = x_dim[index_minx];
+    float fmin_y = y_dim[index_miny];
+    float fmin_z = z_dim[index_minz];
+
+    float fmax_x = x_dim[index_x];
+    float fmax_y = y_dim[index_y];
+    float fmax_z = z_dim[index_z];
+
+    //determine number of voxel cells between min and max of x,y,z
+    int fno_x = 0;
+    int fno_y = 0;
+    int fno_z = 0;
+
+    if (std::fmod((fmax_x - fmin_x), voxel_size) == 0) {
+        fno_x = ((fmax_x - fmin_x) / voxel_size);}
+    else {fno_x = ((fmax_x - fmin_x) / voxel_size) + 1;}
+
+    if (std::fmod((fmax_y - fmin_y), voxel_size) == 0) {
+        fno_y = ((fmax_y - fmin_y) / voxel_size);}
+    else {fno_y = ((fmax_y - fmin_y) / voxel_size) + 1;}
+
+    if (std::fmod((fmax_z - fmin_z), voxel_size) == 0) {
+        fno_z = int((fmax_z - fmin_z) / voxel_size);}
+    else {fno_z = int((fmax_z - fmin_z) / voxel_size) + 1;}
+
+    Rows frows;
+    frows.x = fno_x;
+    frows.y = fno_y;
+    frows.z = fno_z;
+
+    return frows;
+}
+
+Point bbox(const Point& vf0, const Point& vf1, const Point& vf2, float voxel_size) {
+
+    std::vector<Point> fvertices;
+
+    fvertices.emplace_back(vf0);
+    fvertices.emplace_back(vf1);
+    fvertices.emplace_back(vf2);
+
+    //check
 
     // creating vector for each dimension x,y,z
     std::vector<float> x_dim;
     std::vector<float> y_dim;
     std::vector<float> z_dim;
 
-    for (std::vector<Point>::const_iterator i = vertices.begin(); i != vertices.end(); ++i) {
-        //std::cout << i[0][1]<<' ';
+    for (std::vector<Point>::const_iterator i = fvertices.begin(); i != fvertices.end(); ++i) {
         x_dim.push_back(i[0][0]);
         y_dim.push_back(i[0][1]);
         z_dim.push_back(i[0][2]);
@@ -114,10 +211,6 @@ int main(int argc, const char* argv[]) {
     float max_y = y_dim[index_y];
     float max_z = z_dim[index_z];
 
-
-    std::cout << "max_x: " << max_x << '\n' << "max_y: " << max_y << '\n' << "max_z: " << max_z << '\n' << '\n';
-    std::cout << "min_x: " << min_x << '\n' << "min_y: " << min_y << '\n' << "min_z: " << min_z;
-
     //determine number of voxel cells between min and max of x,y,z
     int no_x = 0;
     int no_y = 0;
@@ -136,7 +229,7 @@ int main(int argc, const char* argv[]) {
     else {
         no_y = ((max_y - min_y) / voxel_size) + 1;
     }
-    std::cout << '\n' << no_y;
+    //std::cout << '\n'<< no_y;
 
     if (std::fmod((max_z - min_z), voxel_size) == 0) {
         no_z = int((max_z - min_z) / voxel_size);
@@ -145,56 +238,109 @@ int main(int argc, const char* argv[]) {
         no_z = int((max_z - min_z) / voxel_size) + 1;
     }
 
-    std::cout << '\n' << "x:  " << no_x << "  y: " << no_y << "   z:  " << no_z;
+    Point bbox_min;
+    bbox_min.x = min_x;
+    bbox_min.y = min_y;
+    bbox_min.z = min_z;
 
-    // create vector for bbox
-    std::vector<Point> bbox;
-    bbox.emplace_back(min_x, min_y, min_z);
-    bbox.emplace_back(min_x + no_x * voxel_size, min_y + no_y * voxel_size, min_z + no_z * voxel_size);
+    return bbox_min;
+}
 
-    for (std::vector<Point>::const_iterator i = bbox.begin(); i != bbox.end(); ++i) {
-        std::cout << '\n' << *i << ' ';
+bool intersects(const Point &orig, const Point &dest, const Point &v0, const Point &v1, const Point &v2) {
+    if (signed_volume(v0, v1, v2, orig) > 0 && signed_volume(v0, v1, v2, dest) < 0) {
+        return true;
+    } //next if statement
+    else if (signed_volume(v0, v1, v2, orig) < 0 && signed_volume(v0, v1, v2, dest) > 0) {
+        return true;} //next if statement
+    else { return false; }
+}
+
+int main(int argc, const char* argv[]) {
+    // I have added the path to the obj file. Local path so static and not dynamic.
+    const char* file_in = "C:\\Users\\simon\\Desktop\\Stuff\\1. TU Delft\\2. Semester\\3. GEO1004 3D Modelling of the Built Environment\\1. Assignment\\repo\\geo1004_hw01\\bag_bk.obj";
+    const char* file_out = "vox.obj";
+    float voxel_size = 20.0;
+
+    // Read file
+    std::vector<Point> vertices;
+    std::vector<std::vector<unsigned int>> faces;
+    std::string line;
+    //std::string type;
+    std::ifstream in(file_in, std::ios::in);
+
+    if (!in)
+    {
+        std::cerr << "Cannot open" << file_in << std::endl;
+        exit(1);
     }
 
-    // Create rows from the boudningbox
-    // change to no_x, no_y, no_z but limited to 20 now for the sake of computation time.
-    Rows rows;
-    rows.x = 20;
-    rows.y = 20;
-    rows.z = 20;
+    while (std::getline(in, line))
+    {
+        std::istringstream vert(line);
+        vert >> line;
+        if (line == "v") {
+            float x, y, z;
+            vert >> x >> y >> z;
+            vertices.emplace_back(x, y, z);
+        }
+        if (line == "f") {
+            unsigned int f1, f2, f3;
+            vert >> f1 >> f2 >> f3;
+            std::vector<unsigned int> temp = { f1, f2, f3 };
+            faces.push_back(temp);
+        }
+    }
 
-    // Create the grid with just this line of code? yes but only a grid consisting out of indices
-    VoxelGrid voxels(rows.x, rows.y, rows.z);
-    
-    //const Point& orig, const Point& dest, const Point& v0, const Point& v1, const Point& v2);
+    // Set some iteration variables
 
-    orig.x = min_x;
-    orig.y = min_y;
-    orig.z = min_z;
+    Point orig1, orig2, orig3;
+    Point dest1, dest2, dest3;
 
-    //bool intersects(const Point & orig, const Point & dest, const Point & v0, const Point & v1, const Point & v2) {
+    int p;
+    p = 0;
 
-    // Iterate over the grid
-    for (int i = 0; i < voxels.max_x; i++) {
-        for (int j = 0; j < voxels.max_y; j++) {
-            for (int k = 0; k < voxels.max_z; k++) {
-                std::cout << '\n' << "x:  " << i << "  y: " << j << "   z:  " << k;
-                for (std::vector<Point>::const_iterator i = faces.begin(); i != faces.end(); ++i) {
-                    if intersects(orig, xxxxxxxxx) = 1 {
-                        store the voxel(indices.x, y, z)
+    for (int t = 0; t < 100; t++) { //100 should be changed to max face which is somewhere in the 4000
+        int Iv0 = faces[t][0];
+        int Iv1 = faces[t][1];
+        int Iv2 = faces[t][2];
+        Point v0 = vertices[Iv0];
+        Point v1 = vertices[Iv1];
+        Point v2 = vertices[Iv2];
+
+        Point min;
+        float min_x, min_y, min_z;
+        min = bbox(v0, v1, v2, voxel_size);
+        min_x = min.x;
+        min_y = min.y;
+        min_z = min.z;
+
+        Rows test = face_rows(v0, v1, v2, voxel_size);
+        VoxelGrid voxels(test.x, test.y, test.z);
+
+        for (int i = 0; i < voxels.max_x; i++) {
+            for (int j = 0; j < voxels.max_y; j++) {
+                for (int k = 0; k < voxels.max_z; k++) {
+                    orig1, dest1 = target1(voxel_size, i, j, k, min_x, min_y, min_z);
+                    orig2, dest2 = target2(voxel_size, i, j, k, min_x, min_y, min_z);
+                    orig3, dest3 = target3(voxel_size, i, j, k, min_x, min_y, min_z);
+                    p++;
+                    if (intersects(orig1, dest1, v0, v1, v2)) {
+                        std::cout << '\n' << " #p " << p << " left-right intersection";
+                        //check for silvers
                     }
-                    else go to next one 
+                    else if (intersects(orig2, dest2, v0, v1, v2)) {
+                        std::cout << '\n' << " #p " << p << " front-back intersection";
+                        //check for silvers
+                    }
+                    else if (intersects(orig3, dest3, v0, v1, v2)) {
+                        std::cout << '\n' << " #p " << p << " bottom-top intersection";
+                        //check for silvers
+                    }
+                    else { std::cout << '\n' << " #p " << p << " no intersection"; }
                 }
             }
         }
-
     }
-
-    voxel(5,6,9)
-
-    x_center = min_x + voxel_size*5 + 0.5*voxel_size 
-    y_center = min_y + voxel_size*6 + 0.5*voxel_size 
-    Z_center = min_z + voxel_size*9 + 0.5*voxel_size
 
     // Voxelise
     // Meaby this is what I did abovve but than different?
